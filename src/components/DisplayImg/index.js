ef6924d7762092289c12880ef09620d195ad151b
MatrixShifter <vinov042007@gmail.com>
2026-07-30 15:30:53 +0000

    remove Unnesscary

--- src/components/DisplayImg/index.js
+++
@@ -1,9 +0,0 @@
-import './index.css'
-
-const DisplayImg = ({displayImg}) => (
-  <div className="display-image-container">
-    <img src={displayImg.imageUrl} alt="match" className="display-image" />
-  </div>
-)
-
-export default DisplayImg
